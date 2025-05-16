1c6240676188a4f5317ae3a97833bae910f10845
William Chang <w43chang@uwaterloo.ca>
2025-05-15 19:26:35 -0700

    removed radix

--- components/about.tsx
+++ components/about.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
-import { Card, CardContent, CardTitle } from "./ui/card";
+
 
 export default function About() {
   const { ref } = useSectionInView("About");
